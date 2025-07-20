26e8d4be0bd043398901e42296244b90d0609195
mladicstefan <mladicstefan6@gmail.com>
2025-07-20 13:39:50 +0200

    file reading & output

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
-use std::{fs, panic};
+use std::fs::{self, ReadDir};
+
+fn get_file_metadata(files: ReadDir) {
+    for file in files {
+        let file = match file {
+            Ok(f) => f,
+            Err(e) => {
+                eprintln!("{}", e);
+                continue;
+            }
+        };
+        let file_metadata = match file.metadata() {
+            Ok(meta) => meta,
+            Err(e) => {
+                eprintln!("{}", e);
+                continue;
+            }
+        };
+        println!(
+            "{:?} - {:?} - {:?} - {:?}",
+            file.file_name(),
+            file_metadata.permissions(),
+            file_metadata.modified(),
+            file_metadata.file_type(),
+        );
+    }
+}
 
 fn main() {
-    let path = std::env::home_dir().unwrap().join("Downloads");
+    let path = std::env::current_dir().expect("Couldn't find current dir");
+    println!("Running in dir {:?}", &path);
 
-    let metadata = match fs::metadata(&path) {
-        Ok(meta) => meta,
-        Err(e) => panic!("{:?}", e),
-    };
+    let metadata = fs::metadata(&path).unwrap();
+
     println!("{:?}", metadata);
 
     let files = fs::read_dir(&path).unwrap();
-
-    for file in files {
-        let file = file.unwrap();
-        let file_metadata = file.metadata().unwrap();
-        println!("{:?} - {:?}", file.file_name(), file_metadata);
-    }
+    get_file_metadata(files);
 }
